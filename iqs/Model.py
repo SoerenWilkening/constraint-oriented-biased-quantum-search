aab6a9f603433b01088e1af71635d6544ede2f4c
sorenwilkening <soerenwilkening@gmx.de>
2025-08-14 10:48:56 +0200

    higher standard stopping time for local search

--- iqs/Model.py
+++ iqs/Model.py
@@ -287,7 +287,7 @@ or {self.runtime}s sampling
 		# except:
 		# 	pass
 
-	def local_search(self, distance = 2, callback = None, stop_time = 100, max_worse_acceptances: int = 10):
+	def local_search(self, distance = 2, callback = None, stop_time = 1 << 8, max_worse_acceptances: int = 10):
 		if not self.initial_state: self.manual_initial(0, [0] * self.n)
 		t1 = time()
 		self.final_state = run_local_search(self.initial_state, self.constraint, self.objective, distance, stop_time, self.solver, -1, callback, max_worse_acceptances)
