5617801cac7f17309907697e79cb80c51da93bf9
sorenwilkening <soerenwilkening@gmx.de>
2026-02-05 13:27:36 +0000

    feat(03-02): add solver_ctx_t *ctx to solver.h function signatures
    - Add #include "solver_ctx.h"
    - Update CSearch_opt, CSearch_opt_sat, CSearch_sat signatures
    - Update CSearch_*_monte_carlo_sampler signatures
    - All CSearch_* functions now take ctx as first parameter

--- cbqs/src/solver.h
+++ cbqs/src/solver.h
@@ -12,6 +12,7 @@
 #include "constraint.h"
 #include "state.h"
 #include "model.h"
+#include "solver_ctx.h"
 
 int update_potentials(new_constraints_t *con, int64_t *potentials, int direction, int64_t *ret_total);
 
@@ -27,19 +28,19 @@ int look_ahead_correct(int index, int next_assignment, int depth, int *count_sol
 
 int initial_state_preparation(model_t *mod);
 
-int CSearch_opt(state_t *cur_sol, int j,
+int CSearch_opt(solver_ctx_t *ctx, state_t *cur_sol, int j,
                 new_constraints_t *con, new_constraints_t *obj,
                 int depth_look_ahead, int direction, array_t *ful,
                 int *samples
 );
 
-int CSearch_opt_sat(state_t *cur_sol, int j,
+int CSearch_opt_sat(solver_ctx_t *ctx, state_t *cur_sol, int j,
                     new_constraints_t *con, new_constraints_t *obj,
                     int depth_look_ahead, int direction, array_t *ful,
                     int *samples
 );
 
-int CSearch_sat(state_t *cur_sol, int j,
+int CSearch_sat(solver_ctx_t *ctx, state_t *cur_sol, int j,
                 new_constraints_t *con, new_constraints_t *obj,
                 int depth_look_ahead, int direction, array_t *ful,
                 int *samples
@@ -47,12 +48,14 @@ int CSearch_sat(state_t *cur_sol, int j,
 
 
 double CSearch_opt_monte_carlo_sampler(
-    state_t *cur_sol, new_constraints_t *con, new_constraints_t *obj, double error, int initial_samples);
+    solver_ctx_t *ctx, state_t *cur_sol, new_constraints_t *con, new_constraints_t *obj,
+    double error, int initial_samples);
 
 double CSearch_opt_sat_monte_carlo_sampler(
-    state_t *cur_sol, new_constraints_t *con, new_constraints_t *obj, double error, int direction, int initial_samples);
+    solver_ctx_t *ctx, state_t *cur_sol, new_constraints_t *con, new_constraints_t *obj,
+    double error, int direction, int initial_samples);
 
 double CSearch_sat_monte_carlo_sampler(
-    state_t *cur_sol, new_constraints_t *con, double error, int initial_samples);
+    solver_ctx_t *ctx, state_t *cur_sol, new_constraints_t *con, double error, int initial_samples);
 
 #endif // SOLVER_H
